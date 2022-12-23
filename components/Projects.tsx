ae4a831e40b104658bbe9295260046673004da61
Mehrdad roienyan <100929958+mehrdad672@users.noreply.github.com>
2022-12-23 13:07:49 +0330

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = (props: Props) => {
            <Project image='https://s2.uupload.ir/files/snappfood_0n1e.png' title='Snappfood clone' description={descriptions[1]} link='https://snappfoodclone-52ab4.web.app/' />
           
         </div>
-        <div className="w-full absolute top-[30%] md:top-[10%] bg-[#F7AB0A]/10 left-0 h-[200px] lg:h-[300px] -skew-y-12"></div>
+        <div className="w-full  absolute top-[30%] md:top-[10%] bg-[#F7AB0A]/10 left-0 h-[200px] lg:h-[300px] -skew-y-12"></div>
     </div>
   )
 }
